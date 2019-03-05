044a633480421e52c2239eeb5848a57ff30073da
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-04 21:45:43 -0600

    pushing js

--- main.js
+++ main.js
@@ -31,3 +31,47 @@ console.log(quote.indexOf('is'));
 
 
 //tells the index of the start of the string
+const bucketOfCats = {
+    cat1: 'fluffy',
+    cat2: 5,
+    cat3: cat = () => {
+        return 'cat';
+    },
+    cat4: {
+        water: 'nope',
+        sunlight: 'yes'
+    }
+}
+console.log(bucketOfCats.cat2);
+console.log(bucketOfCats['cat2']);
+console.log(bucketOfCats.cat4.water);
+let hitchhikers_guide = {
+       characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+       catchphrase: "Don't Panic",
+       random_facts: {
+           copies_sold: 14000000,
+           formats: ["radio", "TV", "film", "graphic novel"],
+           ultimate_answer: {
+               meaning_of_life: 42
+           }
+       }
+   };
+   console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);
+   let employee = {
+    name: "Jeff Winger",
+    age: 37,
+    department: "legal",
+    hire_date: "09/22/2010"
+  }
+  console.log(`Our company's lawyer is ${employee.name}.`);
+  console.log(`Jeff was hired on ${employee['hire_date']}.`);
+  
+  employee['vacation-days'] = 20;
+  console.log(employee['vacation-days']);
+  let eom = "employee_of_the_month";
+  employee['eom'] = false;
+  console.log(employee['eom']);
+printToDom('objects', `Our company's lawyer is ${employee.name}.`);
+printToDom('objects', `Jeff was hired on ${employee['hire_date']}.`);
+printToDom('objects', employee['vacation-days']);
+printToDom('objects', employee['eom']);
